45e53eb7eddd72b247da607398c4c335ad745400
Marius Octavin Matei <marius@digitalcareplanning.com>
2019-11-15 15:46:58 +0000

    Started implementing the build hook

--- index.js
+++ index.js
@@ -1,14 +1,97 @@
+const { spawnSync } = require("child_process");
+
+const DEFAULT_DOCKER_TAG = "0.0.1-swift-5.1.2";
+const SWIFT_RUNTIME = "swift";
+const BASE_RUNTIME = "provided";
+
 class SwiftPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
+    this.servicePath = this.serverless.config.servicePath || "";
     this.options = options;
     this.hooks = {
-      "before:package:createDeploymentArtifacts": this.check.bind(this)
+      "before:package:createDeploymentArtifacts": this.build.bind(this)
     };
+    this.custom = Object.assign(
+      { dockerTag: DEFAULT_DOCKER_TAG },
+      this.serverless.service.custom.swift || {}
+    );
   }
 
-  check() {
-    console.log("Before Deploy Resources");
+  runDocker(funcArgs) {
+    const defaultArgs = [
+      "run",
+      "--rm",
+      "-v",
+      `${this.servicePath}:/src`,
+      "--workdir",
+      "/src",
+      `swift build --configuration release --build-path .build-serverless`
+    ];
+
+    const dockerTag = (funcArgs || {}).dockerTag || this.custom.dockerTag;
+    return spawnSync("docker", [
+      ...defaultArgs,
+      `mariusomdev/lambda-swift:${dockerTag}`
+    ]);
+  }
+
+  // Usefull when deploying a specific function
+  functions() {
+    if (this.options.function) {
+      return [this.options.function];
+    } else {
+      return this.serverless.service.getAllFunctions();
+    }
+  }
+
+  build() {
+    const service = this.serverless.service;
+
+    if (service.provider.name != "aws") {
+      return;
+    }
+
+    let swiftFunctionFound = false;
+
+    this.functions().forEach(funcName => {
+      const func = service.getFunction(funcName);
+      const runtime = func.runtime || service.provider.runtime;
+
+      // Skip function which don't apply to swift
+      if (runtime != SWIFT_RUNTIME) {
+        return;
+      }
+
+      swiftFunctionFound = true;
+
+      // Compile here using docker
+      this.serverless.cli.log(`Building native swift ${func.handler} func...`);
+      const res = this.runDocker(func.swift);
+      if (res.error || res.status > 0) {
+        this.serverless.cli.log(
+          `Dockerized swift build encountered an error: ${res.error} ${res.status}.`
+        );
+        throw new Error(res.error);
+      }
+
+      throw new Error("Implemented until here");
+
+      // Ensure the runtime is set to a sane value for other plugins
+      if (func.runtime == SWIFT_RUNTIME) {
+        func.runtime = BASE_RUNTIME;
+      }
+    });
+
+    if (service.provided.runtime == SWIFT_RUNTIME) {
+      service.provider.runtime = BASE_RUNTIME;
+    }
+
+    if (!swiftFunctionFound) {
+      throw new Error(
+        `Error: no Swift functions found. Use 'runtime ${SWIFT_RUNTIME}' in global or function configuration to use this plugin.`
+      );
+    }
   }
 }
 
